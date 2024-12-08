0317ba4d2ce66c505448907799c2023a795f3bdb
yosspow <elhabachi.youssef@arkx.academy>
2024-12-08 12:44:16 +0100

    change db connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDb = () => {
   mongoose
-    .connect("mongodb://localhost:27017/lolo")
+    .connect("mongodb://localhost:27017/sales-management")
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to MongoDb", err));
 };
